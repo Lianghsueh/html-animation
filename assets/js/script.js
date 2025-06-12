bf3ba5be70381de301fc58d5b717e5e765ae4e14
梁家學 <liangjiaxue@liangzhiqingdeMacBook-Air.local>
2025-06-12 12:53:19 +0800

    首次上傳動畫網頁

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById("showBtn");
+const img = document.getElementById("targetImg");
+btn.addEventListener("click", () =>{
+  img.classList.remove("hidden");
+  gsap.fromTo(img,
+    { opacity: 0, y: -50},
+    { opacity: 1, y: 0,duration: 1 }
+  );
+});
